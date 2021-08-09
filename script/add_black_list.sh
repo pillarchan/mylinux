3afb8b7466b9b38cc2cc4e12e8d90622c3e36aa5
pillarchan <57823816+pillarchan@users.noreply.github.com>
2021-08-09 15:52:00 +0800

    Update add_black_list.sh

--- script/add_black_list.sh
+++ script/add_black_list.sh
@@ -2,7 +2,7 @@ log_path="/www/wwwlogs/*.log"
 num=30
 white_ip_list=(47.56.174.7 60.217.248 103.219.176 123.129.224 110.42.2 117.24.15 110.80.137 45.248.10 45.248.9 27.159.65 103.88.34 104.21.27.34 172.67.168.221 119.8 159.138 94.74 104.214 20.187 20.189 52.139 52.175 168.63 13 40.83 207.46 34 35 18.166 18.163 18.162 112.209 86.99 86.98 119.8.17 119.8.16 2.51 175.176 112.206 112.211 114.108 180.190 180.191 86.96 94.204 86.97 47.56.146.87 47.242 8.210 117.24.14 112.5.37 45.251.10 103.64.12.209 110.54 2.49 92.98)
 function send_tg {
-	TELEGRAM_BOT_TOKEN="1105802737:AAHPE1C6YYWbcVuUNsrI0ZeS8ShswTUaMmo"
+	TELEGRAM_BOT_TOKEN="hehehe"
 	local data=$(curl -s "http://ip-api.com/json/$1?lang=zh-CN" | jq .)
 	country=$(echo $data | jq .country)
 	regionName=$(echo $data | jq .regionName)
