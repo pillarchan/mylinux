fe5bf6eadb7464984af1ecc112aebea36d8df3b3
pillarchan <504208247@qq.com>
2023-09-20 07:56:21 +0800

    exp

---
+++ experience/acme/creat_cert.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ $# -eq 1 ];then
+    if [[ -z "$#" ]];then
+        echo "域名不能为空"
+        exit 2
+    elif [[ $(echo "$1" | grep -E '^\w+\.\w+\.\w+$') != "$1" ]];then
+        echo "域名格式为 www.example.com"
+        exit 2
+    else
+        cert_dir="/usr/local/openresty/nginx/cert/$1"
+        mkdir $cert_dir
+        /root/.acme.sh/acme.sh --issue --dns dns_aws -d $1
+        sleep 10
+        /root/.acme.sh/acme.sh --install-cert -d $1 \
+                        --key-file $cert_dir/key.pem \
+                        --fullchain-file $cert_dir/cert.pem \
+                        --reloadcmd "nginx -s reload"
+    fi
+else 
+    echo "请输入一个域名"
+    exit 2;
+fi
--- experience/aws/aws_change_ip_nf.sh
+++ experience/aws/aws_change_ip_nf.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-InstanceId="i-095ca854ac7743cea"
+InstanceId="i-0dc7ffe31c002cfa7"
 OldPublicIpAddress=$(aws ec2 describe-instances --filters "Name=instance-id,Values=$InstanceId" --query  "Reservations[*].Instances[*].NetworkInterfaces[0].Association.PublicIp" --output text)
 NetworkInterfaceId=$(aws ec2 describe-instances --filters "Name=instance-id,Values=$InstanceId" --query  "Reservations[*].Instances[*].NetworkInterfaces[0].NetworkInterfaceId" --output text)
+PrivateIpAddress=$(aws ec2 describe-instances --filters "Name=instance-id,Values=$InstanceId" --query  "Reservations[*].Instances[*].NetworkInterfaces[0].PrivateIpAddress" --output text)
 NewAllocationId=$(aws ec2 allocate-address --domain vpc --query "AllocationId" --output text)
-aws ec2 associate-address --allocation-id $NewAllocationId --network-interface-id NetworkInterfaceId
+aws ec2 associate-address --allocation-id $NewAllocationId --network-interface-id $NetworkInterfaceId --private-ip-address $PrivateIpAddress
 aws ec2 release-address --allocation-id $(aws ec2 describe-addresses --filters "Name=public-ip,Values=$OldPublicIpAddress" --query "Addresses[*].[AllocationId]" --output text)
-NewPublicIp=aws ec2 describe-addresses --filters "Name=network-interface-id,Values=$NetworkInterfaceId" --query "Addresses[*].PublicIp" --output text
+NewPublicIp=$(aws ec2 describe-addresses --filters "Name=network-interface-id,Values=$NetworkInterfaceId" --filters "Name=private-ip-address,Values=$PrivateIpAddress" --query 'Addresses[*].PublicIp' --output text)
 echo "New Elastic IP: "$NewPublicIp
