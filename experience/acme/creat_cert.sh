8c5fe92ac7172322c81f176b0e706af98b9ad771
pillarchan <504208247@qq.com>
2023-10-09 13:48:06 +0800

    ali_cert

--- experience/acme/creat_cert.sh
+++ experience/acme/creat_cert.sh
@@ -3,13 +3,13 @@ if [ $# -eq 1 ];then
     if [[ -z "$#" ]];then
         echo "域名不能为空"
         exit 2
-    elif [[ $(echo "$1" | grep -E '^\w+\.\w+\.\w+$') != "$1" ]];then
-        echo "域名格式为 www.example.com"
+    elif [[ $(echo "$1" | grep -E '^(\*|\w+)(\.)?\w+\.\w+$') != "$1" ]];then
+        echo "域名格式为 www.example.com或example.com"
         exit 2
     else
         cert_dir="/usr/local/openresty/nginx/cert/$1"
         mkdir $cert_dir
-        /root/.acme.sh/acme.sh --issue --dns dns_aws -d $1
+        /root/.acme.sh/acme.sh --issue --dns dns_ali -d $1
         sleep 10
         /root/.acme.sh/acme.sh --install-cert -d $1 \
                         --key-file $cert_dir/key.pem \
